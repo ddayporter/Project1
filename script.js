795540fa9318cd64a741fbd76db13fbdc3571c2c
Rachel Porter <rachelandrewsporter@gmail.com>
2015-10-27 15:29:44 -0400

    cleans up styles and formatting and minor tweaks to functionality

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 $( document ).ready( function () {
   $(".answerField").hide();
+  $(".reset").hide();
+  $(".results").hide();
   $(".play").on("click", triviaStart);
 }); //document.ready function closure
 
@@ -13,75 +15,86 @@ var easyQuestions = [
     question: "Which of the main characters accepts a gift from Mordeth?",
     options: "<p>a) Rand</p><p>b) Mat</p><p>c) Perrin</p><p>d) Moiraine</p>",
     answer: "b",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Dagger-icon.svg"
   },
 
   {
     question: "How does Rand help pay for room and board at the inns he and Mat visit on the way to Caemlyn?",
     options: "<p>a) he helps out in the forge</p>   <p>b) he juggles to entertain the guests</p>   <p>c) he plays the flute to entertain the guests</p>   <p>d) he uses the One Power to do tricks to entertain the guests</p>",
     answer: "c",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Deadtree-icon.svg"
   },
 
   {
     question: "What is the name of the place where the Dark One is imprisoned?",
     options: "<p>a) Shayol Ghul</p>   <p>b) Ankor Dall</p>   <p>c) Fal Moran</p>   <p>d) Seanchan</p>",
     answer: "a",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Blight-icon.svg"
   },
 
   {
     question: "During what festival is the first attack by Trollocs in Emond's Field?",
     options: "<p>a) Feast of Lights</p>   <p>b) Festival of Birds</p>   <p>c) Bel Tine</p>   <p>d) Michaelmas</p>",
     answer: "c",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Aessedai-icon.svg"
   },
 
   {
     question: "What skill does Perrin learn he possesses on the journey to Caemlyn?",
     options: "<p>a) he can communicate with ravens</p>   <p>b) he can juggle</p>   <p>c) he can channel the One Power</p>   <p>d) he can communicate with wolves</p>",
     answer: "d",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Falcon-icon.svg"
   },
 
   {
-    question: "What question does the leader of a Tua'athon caravan always ask visitors?",
+    question: "What question does the leader of a Tuatha'an caravan always ask visitors?",
     options: "<p>a) What is your name?</p>   <p>b) Do you know the song?</p>   <p>c) Are you carrying any iron?</p>   <p>d) Do you know the dance?</p>",
     answer: "b",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Leaves-icon.svg"
   },
 
   {
     question: "What is the killing fog of Shadar Logoth known as?",
     options: "<p>a) Mashadar</p>   <p>b) Mashiara</p>   <p>c) Machin Shin</p>   <p>d) Moghedien</p>",
     answer: "a",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Ravens-icon.svg"
   },
 
   {
     question: "What is the name of the Green Man who protected the Eye of the World?",
     options: "<p>a) Da'shain</p>   <p>b)  Haran</p>   <p>c) Someshta</p>   <p>d) Loial</p>",
     answer: "c",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Leaf-icon.svg"
   },
 
   {
     question: "What is the meaning of the word <em>ta'veren</em>?",
     options: "<p>a) another word for the True Source</p>   <p>b) an item that allows a person who can channel to tap into more power than they could handle otherwise</p>   <p>c) people who have pledged to never commit an act of violence</p>   <p>d) people around whom the Wheel of Time weaves the Pattern</p>",
     answer: "d",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Snake-icon.svg"
   },
 
   {
     question: "What does the group find at the Eye when they first arrive?",
     options: "<p>a) an Aielman in a cage</p>   <p>b) a crown of swords</p>  <p>c) a well of <em>saidin</em></p>   <p>d) a powerful <em>angreal</em></p>",
     answer: "c",
-    points: 2
+    points: 2,
+    imageURL: "http://jcsalomon.github.io/wot-chapter-icons/Fang-icon.svg"
   }
 ]
 
 function triviaStart() {
-  $(".play").off();
+  $(".play").hide();
+  $(".results").show();
   $(".reset").on("click", resetGame);
   firstQuestion();
 
@@ -89,6 +102,7 @@ function triviaStart() {
 
 function firstQuestion() {
   console.log("first question");
+  $(".decoration").attr("src", easyQuestions[0].imageURL)
   $(".housekeeping").html("Question 1/10:");
   $(".question").html(easyQuestions[0].question);
   $(".options").html(easyQuestions[0].options);
@@ -97,6 +111,7 @@ function firstQuestion() {
 }
 
 function printQuestion() {
+  // $(".decoration").attr("src", easyQuestions[i].imageURL)
   $(".housekeeping").html("Question " + (i+1) + "/10:");
   $(".question").html(easyQuestions[i].question);
   $(".options").html(easyQuestions[i].options);
@@ -109,11 +124,11 @@ function printAnswer() {
   console.log("Input value is " + $('input').val() + ".");
   console.log("Value of i is currently " + i + ".");
   if ($('input').val() == easyQuestions[i].answer) {
-    $(".answer").html("Correct!");
+    $(".answer").addClass("correctAnswer").html("Correct!");
     score = score + easyQuestions[i].points;
     console.log("Right answer. Score is " + score);
   } else {
-    $(".answer").html("Sorry. You answered " + $('input').val() + ", but the correct answer is " + easyQuestions[i].answer + ".");
+    $(".answer").addClass("wrongAnswer").html("Sorry. You answered " + $('input').val() + ", but the correct answer is " + easyQuestions[i].answer + ".");
     score = score;
     console.log("wrong answer. Score is " + score);
   }
@@ -168,7 +183,7 @@ function resetGame() {
   $('.board').show();
   $("main > p").remove();
   $(".next").off();
-  $(".play").on("click", triviaStart);
+  $(".play").show();
 }
 
 
