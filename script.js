59062515f79c9bc43366b4d735442eea33a10907
Rachel Porter <rachelandrewsporter@gmail.com>
2015-10-28 11:51:07 -0400

    fixes error of reset button not appearing with triviaEnd()

--- script.js
+++ script.js
@@ -140,6 +140,7 @@ function printAnswer() {
 function nextQuestion() {
   $(".answer").removeClass("correctAnswer wrongAnswer");
   $(".next").off();
+  $(".reset").show();
   $(".answer").html("");
   $("input").val("");
   console.log("Next button clicked");
@@ -160,7 +161,6 @@ function triviaEnd() {
   $(document.createElement('p')).addClass("final").html('Your final score was ' + score + '.').appendTo('main');
   toggleMessage();
   $(document.createElement('p')).html('Thanks for playing! To play again click "Reset"').appendTo('main');
-  $(".reset").show();
 }
 
 function toggleMessage() {
@@ -189,9 +189,9 @@ function resetGame() {
   $(".play").show();
 }
 
-function toggleImage() {
-
-}
+// function toggleImage() {
+// display an image from an array of images - image determined by final score.
+// }
 
 // more advanced features to add:
 // **1 $('.next').html('FINISH').on("click", function() { <-- want Next button
